8c313047c5ac54d8f7107caed660393d8506ac5f
kouyou.sbudiman@gmail.com <kouyou.sbudiman@gmail.com>
2024-05-29 18:29:11 +0900

    StageScript こうしん

--- Assets/01_Game/UI Scripts/StageMenu.cs
+++ Assets/01_Game/UI Scripts/StageMenu.cs
@@ -13,15 +13,7 @@ public class StageMenu : MonoBehaviour
     private void Awake()
     {
         ButtonsToArray();
-        int unlockedStage = PlayerPrefs.GetInt("unlockedStage", 1);
-        for (int i = 0; i < buttons.Length; i++)
-        {
-            buttons[i].interactable = false;
-        }
-        for (int i = 0;i<unlockedStage; i++)
-        {
-            buttons[i].interactable = true;
-        }
+        
     }
     public void OpenStage(int stageId)
     {
